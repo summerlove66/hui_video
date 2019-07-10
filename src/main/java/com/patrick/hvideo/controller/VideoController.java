9db0da4726df218595c06de89f47cb998e53d8eb
Hui <coderslash@gmail.com>
2019-07-10 20:04:16 +0800

    base64 加密链接 前端解密 +  增加全局错误处理

--- src/main/java/com/patrick/hvideo/controller/VideoController.java
+++ src/main/java/com/patrick/hvideo/controller/VideoController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
@@ -22,61 +23,61 @@ public class VideoController {
     private VideoColumnMapper videoColumnMapper;
 
     @Value("${page.size}")
-    private  int pageSize;
+    private int pageSize;
 
     @GetMapping("/video/{videoId}")
 
-    public String getVideoByID(@PathVariable int videoId , Model model) {
+    public String getVideoByID(@PathVariable int videoId, Model model) {
         Video video = videoMapper.findById(videoId);
-        if(video == null){
-            video = videoMapper.findById(30000);
-        }
-        VideoColumn videoColumn = videoColumnMapper.findVideoColBYSourceAndDataId(video.getSource(),video.getDataId());
+        video.setLink(Base64.getUrlEncoder().encodeToString(video.getLink().getBytes()));
+        VideoColumn videoColumn = videoColumnMapper.findVideoColBYSourceAndDataId(video.getSource(), video.getDataId());
 
-        List<Video> videoList = videoMapper.findBySourceAndDataId(video.getSource(),video.getDataId());
-        model.addAttribute("video",video);
-        model.addAttribute("videoList" ,videoList);
-        model.addAttribute("videoCol" ,videoColumn);
+        List<Video> videoList = videoMapper.findBySourceAndDataId(video.getSource(), video.getDataId());
+        model.addAttribute("video", video);
+        model.addAttribute("videoList", videoList);
+        model.addAttribute("videoCol", videoColumn);
         return "play";
     }
 
 
-
     @GetMapping("/col/{videoColId}")
 
-    public String getVideoColByID(@PathVariable int videoColId , Model model) {
+    public String getVideoColByID(@PathVariable int videoColId, Model model) {
         VideoColumn videoColumn = videoColumnMapper.findVideoColumnById(videoColId);
 
-        System.out.println("VideoCOL" +videoColumn);
-        List<Video> videoList = videoMapper.findBySourceAndDataId(videoColumn.getSource(),videoColumn.getDataId());
-        model.addAttribute("video",videoList.get(0));
-        model.addAttribute("videoList" ,videoList);
-        model.addAttribute("videoCol" ,videoColumn);
+        System.out.println("VideoCOL" + videoColumn);
+        List<Video> videoList = videoMapper.findBySourceAndDataId(videoColumn.getSource(), videoColumn.getDataId());
+        model.addAttribute("video", videoList.get(0));
+        model.addAttribute("videoList", videoList);
+        model.addAttribute("videoCol", videoColumn);
         return "col";
     }
 
 
-
     @GetMapping("cols")
 
-    public String getVideoColumnByPage(@RequestParam(name = "pn" ,defaultValue ="1" ,required = false) int pageNum  , Model model,
-                                          VideoColumn videoColumn) {
+    public String getVideoColumnByPage(@RequestParam(name = "pn", defaultValue = "1", required = false) int pageNum, Model model,
+                                       VideoColumn videoColumn) {
 
         System.out.println("VIDEOCOLUMN " + videoColumn);
         List<VideoColumn> videoColumnList;
 
-        Page<?> page = PageHelper.startPage(pageNum,pageSize);
+        Page<?> page = PageHelper.startPage(pageNum, pageSize);
         videoColumnList = videoColumnMapper.findVideoColByRange(videoColumn);
-        model.addAttribute("videoColumnList" ,videoColumnList);
+        model.addAttribute("videoColumnList", videoColumnList);
 //        model.addAttribute("pn" ,page.getPageNum());
 
         return "album";
 
     }
 
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public String errorReq(Exception exception) {
 
-
-
+        exception.printStackTrace();
+        return "你访问的东西不存在";
+    }
 
 
 }
--- src/test/java/com/patrick/hvideo/HVideoApplicationTests.java
+++ src/test/java/com/patrick/hvideo/HVideoApplicationTests.java
@@ -5,12 +5,18 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Base64;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HVideoApplicationTests {
 
     @Test
     public void contextLoads() {
+
+//
+//        String originalUrl = "https://www.google.co.nz/?gfe_rd=cr&ei=dzbFV&gws_rd=ssl#q=java";
+//        System.out.println(Base64.getUrlEncoder().encodeToString(originalUrl .getBytes()));
     }
 
 }
